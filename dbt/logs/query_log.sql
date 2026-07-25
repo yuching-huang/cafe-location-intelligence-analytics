3729b8fd3e7c52c3bb3dd988324cbb9c51ebc20d
Jennifer <jenniferhuang87@gmail.com>
2026-07-24 17:26:14 -0700

    initialized dbt project

---
+++ dbt/logs/query_log.sql
@@ -0,0 +1,9 @@
+-- created_at: 2026-07-24T23:54:27.480074+00:00
+-- finished_at: 2026-07-24T23:54:27.595863+00:00
+-- elapsed: 115ms
+-- outcome: success
+-- dialect: snowflake
+-- node_id: not available
+-- query_id: 01c5eeda-020a-22f2-0022-11d7005935de
+-- desc: not provided
+select 1 as id;
