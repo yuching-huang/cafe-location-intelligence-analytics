38f13c4d5a232f4d9d053d659e90b72d018daea7
Jennifer <jenniferhuang87@gmail.com>
2026-07-25 18:27:02 -0700

    create dbt staging models

---
+++ dbt/cafe_intelligence/models/staging/stg_google_nearby_cafes.sql
@@ -0,0 +1,19 @@
+SELECT
+    batch_id,
+    cafe_place_id,
+    amenity_type,
+    search_radius_meters,
+
+    raw_data:id::STRING AS nearby_place_id,
+    raw_data:displayName.text::STRING AS nearby_place_name,
+    raw_data:primaryType::STRING AS nearby_primary_type,
+    raw_data:types AS nearby_types_raw,
+    raw_data:formattedAddress::STRING AS nearby_formatted_address,
+    raw_data:location.latitude::FLOAT AS nearby_latitude,
+    raw_data:location.longitude::FLOAT AS nearby_longitude,
+    raw_data:rating::FLOAT AS nearby_rating,
+    raw_data:userRatingCount::NUMBER AS nearby_review_count,
+
+    source,
+    ingested_at
+FROM {{ source('raw', 'raw_google_nearby_places') }}
---
+++ dbt/cafe_intelligence/models/staging/stg_unique_cafes.sql
@@ -0,0 +1,36 @@
+SELECT
+    cafe_id,
+
+    ANY_VALUE(cafe_name) AS cafe_name,
+    ANY_VALUE(rating) AS rating,
+    ANY_VALUE(review_count) AS review_count,
+    ANY_VALUE(price_level) AS price_level,
+    ANY_VALUE(primary_type) AS primary_type,
+    ANY_VALUE(types_raw) AS types_raw,
+    ANY_VALUE(formatted_address) AS formatted_address,
+    ANY_VALUE(latitude) AS latitude,
+    ANY_VALUE(longitude) AS longitude,
+    ANY_VALUE(weekday_descriptions) AS weekday_descriptions,
+    ANY_VALUE(opening_periods) AS opening_periods,
+    ANY_VALUE(parking_options) AS parking_options,
+
+    ANY_VALUE(serves_breakfast) AS serves_breakfast,
+    ANY_VALUE(serves_brunch) AS serves_brunch,
+    ANY_VALUE(serves_lunch) AS serves_lunch,
+    ANY_VALUE(serves_dinner) AS serves_dinner,
+    ANY_VALUE(serves_dessert) AS serves_dessert,
+    ANY_VALUE(serves_coffee) AS serves_coffee,
+    ANY_VALUE(serves_vegetarian_food) AS serves_vegetarian_food,
+
+    LISTAGG(
+        DISTINCT search_neighborhood,
+        ', '
+    ) AS found_in_neighborhoods,
+
+    MAX(ingested_at) AS latest_ingested_at
+
+FROM {{ ref('stg_google_places_cafes') }}
+
+WHERE cafe_id IS NOT NULL
+
+GROUP BY cafe_id
